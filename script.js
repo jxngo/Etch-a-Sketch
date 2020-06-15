54f383e7645b8199cae365c1a25baeee41451429
Johnny Ngo <johnnyhngo@yahoo.com>
2020-06-14 20:00:18 -0400

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+const containerSqDiv = document.querySelector("#sqDivs");
+makeRows(16,16);
+
+
+
+function makeRows(rows,cols) {
+    containerSqDiv.style.setProperty('--grid-rows', rows);
+    containerSqDiv.style.setProperty('--grid-cols',cols);
+
+    for (c = 0; c < (rows * cols); c++ ){
+        let cell = document.createElement("div");
+        containerSqDiv.appendChild(cell).className="grid-item";
+    };
+};
