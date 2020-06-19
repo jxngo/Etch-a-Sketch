3bb7987a34e04f50b50c51a19f688627a4ad0a0e
Johnny Ngo <johnnyhngo@yahoo.com>
2020-06-19 16:14:18 -0400

    Added stylesheet

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const containerSqDiv = document.querySelector("#sqDivs");
+const resetButton = document.querySelector("#resetBut");
 
 makeRows(16,16);
-
 
 
 function makeRows(rows,cols) {
@@ -12,8 +12,20 @@ function makeRows(rows,cols) {
         let cell = document.createElement("div");
         containerSqDiv.appendChild(cell).className="grid-item";
         cell.addEventListener('mouseenter', (e) => {
-            cell.style.background= "black";
-        })
-        
-    };
-};
+            cell.style.background= "#b5cdbe";
+        })   
+    }
+}
+
+function reset() {
+    let cols, rows;
+    containerSqDiv.innerHTML = '';
+    rows = window.prompt("Enter rows");
+    cols = window.prompt("Enter cols");
+    if (rows !== null || cols !== null)
+    makeRows(rows,cols);
+    else
+        makeRows(16,16);
+}
+
+resetButton.addEventListener("click",reset);
